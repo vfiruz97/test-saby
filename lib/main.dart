f65f67dd81358e0f9d659a6d4e04524f3a201c7a
Firuz <vfiruz97@gmail.com>
2021-08-14 12:30:05 +0500

    aside main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:test/injection.dart';
-import 'package:test/presentation/second_page/second_page.dart';
-import 'package:test/presentation/widgets/dot_button_widget.dart';
+import 'package:test/presentation/main/page_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,77 +19,6 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: const PageViewWidget(),
-    );
-  }
-}
-
-class PageViewWidget extends StatefulWidget {
-  const PageViewWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _PageViewWidgetState createState() => _PageViewWidgetState();
-}
-
-class _PageViewWidgetState extends State<PageViewWidget> {
-  final PageController _pageController = PageController();
-  int currentPageIndex = 0;
-
-  void _jumpToPage(int pageIndex) {
-    if (_pageController.hasClients) {
-      _pageController.animateToPage(
-        pageIndex,
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        onPageChanged: (page) {
-          setState(() {
-            currentPageIndex = page;
-          });
-        },
-        controller: _pageController,
-        children: <Widget>[
-          Center(
-            child: ElevatedButton(
-              onPressed: () => _jumpToPage(1),
-              child: const Text('Next'),
-            ),
-          ),
-          const SecondPage(),
-        ],
-      ),
-      bottomNavigationBar: SizedBox(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DotButtonWidget(
-              showActiveColor: currentPageIndex == 0,
-              onTap: () => _jumpToPage(0),
-            ),
-            const SizedBox(width: 10),
-            DotButtonWidget(
-              showActiveColor: currentPageIndex == 1,
-              onTap: () => _jumpToPage(1),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
